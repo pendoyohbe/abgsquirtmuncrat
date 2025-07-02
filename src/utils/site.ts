4a42bf89e5407eb7b9eb943388fa01415fb198ed
pendoyohbe <pendoyohbe@gmail.com>
2025-07-02 08:47:30 +0700

    Update site.ts

--- src/utils/site.ts
+++ src/utils/site.ts
@@ -1,4 +1,4 @@
-export const nama = 'Sbg Squirt Muncrat';
+export const nama = 'Abg Squirt Muncrat';
 export const desk = 'Video Bokep Indo Bocil Ngentot Jilbab Squirt Montok Smp Mama Live Viral Colmek Sma Hijab Tante China';
 export const url = 'https://abgsquirtmuncrat.pages.dev';
 export const terbit = '2025-01-26T00:01:03Z';
