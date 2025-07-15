a115c7bedcb79a9151c78a2dedc123a539b042d9
pendoyohbe <pendoyohbe@gmail.com>
2025-07-15 16:18:57 +0700

    Add files via upload

---
+++ src/utils/site.js
@@ -0,0 +1,9 @@
+export const nama = 'Abg Squirt Muncrat';
+export const desk = 'Video Bokep Indo Bocil Ngentot Jilbab Squirt Montok Smp Mama Live Viral Colmek Sma Hijab Tante China';
+export const url = 'https://abgsquirtmuncrat.pages.dev';
+export const terbit = '2025-01-26T00:01:03Z';
+
+export const google = 'FE7As5TTlDHaNnUhPCMK9xOsWTuTPyii6uoEVPnfHXc';
+export const yandex = '576fa4d48aa58fb3'
+
+export const index = 'a55aed12-c025-4244-a1e7-889f626578d6';
